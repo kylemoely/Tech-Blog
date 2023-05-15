ba87bb3b21ea8a6c0abd112a6b250cfaeb47f472
kylemoely <kylemoely@gmail.com>
2023-05-14 23:36:38 -0500

    Created Post class and started constructing dashboard page

---
+++ models/Post.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique:true,
+            autoIncrement: true,
+            primaryKey:true
+        },
+        username: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'user',
+                key: 'username'
+            }
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 30]
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        modelName: 'post'
+    }
+)
+
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+User.hasMany(Post, {
+    foreignKey: 'username'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'username'
+});
+
+module.exports = { User, Post };
